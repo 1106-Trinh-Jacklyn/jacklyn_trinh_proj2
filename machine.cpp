bee239f607810e9df3a3858105544301583461d0
1106-Trinh-Jacklyn <144396202+1106-Trinh-Jacklyn@users.noreply.github.com>
2024-10-31 22:52:04 +0000

    commit

--- machine.cpp
+++ machine.cpp
@@ -46,74 +46,129 @@ void Machine::parse(const string& line){
     uint32_t num1 = stoul(hex1, nullptr, 16);
     uint32_t num2 = stoul(hex2, nullptr, 16);
 
-    if(operation == "ADD" || operation == "ADDS"){
+    if(operation == "ADD"){
         addCmd(num1, num2);
     }
-    if(operation == "AND" || operation == "ANDS"){
-        addCmd(num1, num2);
+    if(operation == "ADDS"){
+        addsCmd(num1, num2);
     }
-    if(operation == "ASR" || operation == "ASRS"){
+    if(operation == "AND"){
+        andCmd(num1, num2);
+    }
+    if(operation == "ANDS"){
+        andsCmd(num1, num2);
+    }
+    if(operation == "ASR"){
         asrCmd(num1, num2);
     }
-    if(operation == "LSR" || operation == "LSRS"){
+    if(operation == "ASRS"){
+        asrsCmd(num1, num2);
+    }
+    if(operation == "LSR"){
         lsrCmd(num1, num2);
     }
-    if(operation == "LSL" || operation == "LSLS"){
+    if(operation == "LSRS"){
+        lsrsCmd(num1, num2);
+    }
+    if(operation == "LSL"){
         lslCmd(num1, num2);
     }
-    if(operation == "NOT" || operation == "NOTS"){
+    if(operation == "LSLS"){
+        lslsCmd(num1, num2);
+    }
+    if(operation == "NOT"){
         notCmd(num1);
     }
-    if(operation == "ORR" || operation == "ORRS"){
+    if(operation == "NOTS"){
+        notsCmd(num1);
+    }
+    if(operation == "ORR"){
         orrCmd(num1, num2);
     }
-    if(operation == "SUB" || operation == "SUBS"){
+    if(operation == "ORRS"){
+        orrsCmd(num1, num2);
+    }
+    if(operation == "SUB"){
         subCmd(num1, num2);
     }
-    if(operation == "XOR" || operation == "XORS"){
+    if(operation == "SUBS"){
+        subsCmd(num1, num2);
+    }
+    if(operation == "XOR"){
         xorCmd(num1, num2);
+    }
+    if(operation == "XORS"){
+        xorsCmd(num1, num2);
     }
 }
 
 void Machine::addCmd(uint32_t num1, uint32_t num2){
     uint32_t result = num1 + num2;
+    print(operation, num1, num2, result);
+}
 
-    print();
-
+void Machine::addsCmd(uint32_t num1, uint32_t num2){
+    uint32_t result = num1 + num2;
+    flag(result);
+    print(operation, num1, num2, result);
 }
 
 void Machine::andCmd(uint32_t num1, uint32_t num2){
     uint32_t result = num1 & num2;
+    print(operation, num1, num2, result);
+}
 
-    print();
+void Machine::andsCmd(uint32_t num1, uint32_t num2){
+    uint32_t result = num1 & num2;
+    flag(result);
+    print(operation, num1, num2, result);
 }
 
 void Machine::asrCmd(uint32_t num, int shift){
+    uint32_t result = 
+    print(operation, num1, num2, result);
+}
 
+void Machine::asrsCmd(uint32_t num, int shift){
+    uint32_t result = 
+    flag(result);
+    print(operation, num1, num2, result);
 }
 
 void Machine::lsrCmd(uint32_t num, int shift){
+    uint32 result = 
+    print(operation, num1, num2, result);
+}
 
+void Machine::lsrsCmd(uint32_t num, int shift){
+    uint32 result = 
+    flag(result);
+    print(operation, num1, num2, result);
 }
 
 void Machine::lslCmd(uint32_t num, int shift){
-
+    uint32 result = 
+    print(operation, num1, num2, result);
 }
 
 void Machine::notCmd(uint32_t num){
-
+    uint32 result = 
+    print(operation, num1, num2, result);
 }
 
 void Machine::orrCmd(uint32_t num1, uint32_t num2){
-
+    uint32 result = 
+    print(operation, num1, num2, result);
 }
 
 void Machine::subCmd(uint32_t num1, uint32_t num2){
-
+    uint32 result = 
+    print(operation, num1, num2, result);
 }
 
 void Machine::xorCmd(uint32_t num1, uint32_t num2){
-
+    uint32 result = 
+    print(operation, num1, num2, result);
 }
 
 void Machine::print(string& operation, uint32_t num1, uint32_t num2, uint32_t result){
@@ -121,7 +176,7 @@ void Machine::print(string& operation, uint32_t num1, uint32_t num2, uint32_t re
     cout << "N: " << N << " Z: " << Z << endl;
 }
 
-void Machine::printNot(string& operation, uint32_t num, uint32_t result){
+void Machine::print(string& operation, uint32_t num, uint32_t result){
     cout << operation << " 0x" << hex << uppercase << num << ": 0x" << result << endl;
     cout << "N: " << N << " Z: " << Z << endl;
 }
